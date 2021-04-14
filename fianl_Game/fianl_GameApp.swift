8f7d31c6ff666cfd55f20e1afb7bb60b7e1608e6
c60603 <c60603@@gmail.com>
2021-04-14 15:30:21 +0800

    Initial Commit

---
+++ fianl_Game/fianl_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  fianl_GameApp.swift
+//  fianl_Game
+//
+//  Created by User08 on 2021/4/14.
+//
+
+import SwiftUI
+
+@main
+struct fianl_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
